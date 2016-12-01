88c3e311eb6a49dbf5ee2fe5b5826777750ba835
water.li <water.li@shijinet.com.cn>
2016-12-01 11:35:42 +0800

    init maven project

---
+++ src/main/java/tao/pms/App.java
@@ -0,0 +1,13 @@
+package tao.pms;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
